fc6cad07220aff002ba38b670f928a457b0cdbb2
Efraim Andrade <efraim.dev@gmail.com>
2019-12-25 17:49:53 -0300

    feat: 🎸 Start Project
    added the folder structure, eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['react-app', 'plugin:@typescript-eslint/recommended', 'airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jsx-a11y',
+    'import',
+    'react-hooks',
+    'prettier',
+    'import-helpers',
+  ],
+  rules: {
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          ['/^react/', 'module'],
+          '/^~/',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: { order: 'ignore', ignoreCase: true },
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+interface User {
+  name: string;
+  age: number;
+}
 
 const App: React.FC = () => {
+  const [title, setTitle] = useState<User>();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          <code>
+            {title?.name}
+            {title?.age}
+          </code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,9 +27,14 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+        <input
+          type="text"
+          value={title?.name}
+          onChange={(event) => setTitle({ age: 1, name: event.target.value })}
+        />
       </header>
     </div>
   );
-}
+};
 
 export default App;
