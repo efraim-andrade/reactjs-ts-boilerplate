b73ac3c6ff94a6b3cc8db1d458196335b9b5e769
Efraim Andrade <efraim.dev@gmail.com>
2019-12-25 22:04:26 -0300

    feat: 🎸 Configs
    added the global styles and a example component

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-interface User {
-  name: string;
-  age: number;
-}
+import GlobalStyles from './theme/global';
 
-const App: React.FC = () => {
-  const [title, setTitle] = useState<User>();
 
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>
-            {title?.name}
-            {title?.age}
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <input
-          type="text"
-          value={title?.name}
-          onChange={(event) => setTitle({ age: 1, name: event.target.value })}
-        />
-      </header>
+    <div>
+      <GlobalStyles />
+
+      <h1>iae</h1>
     </div>
   );
-};
-
-export default App;
+}
---
+++ src/components/ExampleComponent/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function ExampleComponent() {
+  return (
+    <Container>
+      <h1>sample</h1>
+    </Container>
+  );
+}
